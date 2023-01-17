1ce0564567417674d88cd2a69fac25a2f84790ff
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2023-01-17 14:28:44 -0600

    currency converter

---
+++ src/main/java/CurrencyConverter.java
@@ -0,0 +1,44 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class CurrencyConverter {
+
+
+    public static double yenToDollar(double yen){
+
+        double dollarAmount = yen*0.15;
+
+        return dollarAmount;
+
+    }
+    public static double dollarToYen(double dollar){
+
+        double yenAmount = dollar*0.15;
+
+        return yenAmount;
+
+    }
+
+
+    public static void main(String[] args) throws Exception{
+//        NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
+////        String value = nf.getCurrency().getDisplayName();
+//        double amount = 123.55;
+//        System.out.println(nf.format(amount));
+
+        NumberFormat yenF = NumberFormat.getCurrencyInstance(Locale.CHINA);
+
+        String yenValue = yenF.getCurrency().getDisplayName();
+        System.out.println(yenValue);
+        double yenAmount1 = 550;
+        System.out.println(yenF.format(yenAmount1));
+
+        double dollar1 = yenToDollar(yenAmount1);
+        System.out.println(dollar1);
+
+
+
+    }
+
+
+}
